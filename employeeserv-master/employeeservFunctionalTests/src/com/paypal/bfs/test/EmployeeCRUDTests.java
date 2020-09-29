282957e301fb2934e63b76a5f59465f37e430861
rrepaka <rrepaka@gmail.com>
2020-09-28 18:52:58 -0700

    test cases

---
+++ employeeserv-master/employeeservFunctionalTests/src/com/paypal/bfs/test/EmployeeCRUDTests.java
@@ -0,0 +1,35 @@
+package com.paypal.bfs.test;
+
+import java.util.logging.Logger;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+
+
+class EmployeeCRUDTests {
+	
+	static Logger logger = Logger.getLogger("EmployeeCRUDTests");
+	
+	@BeforeAll
+	static void setup() {
+		logger.info(" Setting up test cases") ;
+	}
+	
+	
+	@Test
+	void testEmployeeId() {
+		
+	}
+	
+	@Test
+	void createEmploee() {
+		 
+	}
+	
+	
+	
+	
+
+	
+}
